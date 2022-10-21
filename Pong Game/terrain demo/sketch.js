3099b21a011b21cf36dd6bad0eb1d1c4f7673524
Kesur0403463 <0403463@sps-student.sk.ca>
2022-10-21 12:16:21 -0600

    perlin noise demo

---
+++ Pong Game/terrain demo/sketch.js
@@ -0,0 +1,40 @@
+// Terrain Demo
+// Kati
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let theHeights = [];
+let startingLocation = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  theHeights = generateHeights(20000);
+}
+
+function draw() {
+  background(220);
+  for(let i = startingLocation; i < startingLocation + width; i++){
+    displayRectangle(i - startingLocation, theHeights[i], 1);
+  }
+
+  if(keyIsPressed){
+    startingLocation += 5;
+  }
+}
+
+function displayRectangle(x, rectHeight, rectWidth){
+  let y = height - rectHeight;
+  rect(x,y, rectWidth, rectHeight);
+}
+
+function generateHeights(howMany){
+  let tempArray = [];
+  let time = random(1000);
+  for (let i = 0; i < howMany; i++){
+    tempArray.push(noise(time) * height);
+    time += 0.001;
+  }
+  return tempArray;
+}
