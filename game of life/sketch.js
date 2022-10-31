03fc6be4d911a13761f1bb275cb8ac31cf8efe67
Kesur0403463 <0403463@sps-student.sk.ca>
2022-10-31 11:05:31 -0600

    yeah

--- game of life/sketch.js
+++ game of life/sketch.js
@@ -1,10 +1,18 @@
 // Game of Life
 // Kati Kesur
-let ROWS = 40;
-let COLS = 55;
+
+
+const ROWS = 40;
+const COLS = 40;
 let grid;
 let cellWidth;
 let cellHeight;
+let autoPlay = false;
+let gosperGun;
+
+function preload() {
+  gosperGun = loadJSON("gosper.json");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,7 +23,81 @@ function setup() {
 
 function draw() {
   background(220);
+  if (autoPlay && frameCount % 3 === 0) {
+    grid = takeTurn(grid);
+  }
   displayGrid(grid);
+}
+
+function keyPressed() {
+  if (key === "e") {
+    grid = create2dArray(COLS, ROWS);
+  }
+  if (key === " ") {
+    grid = takeTurn(grid);
+  }
+  if (key === "a") {
+    autoPlay = !autoPlay;
+  }
+  if (key === "g") {
+    grid = gosperGun;
+  }
+}
+
+function takeTurn(grid) {
+  let nextTurn = create2dArray(COLS, ROWS);
+
+  for (let y=0; y<ROWS; y++) {
+    for (let x=0; x<COLS; x++) {
+      let neighbours = 0;
+
+      //look at all cells around this one...
+      for (let i=-1; i<=1; i++) {
+        for (let j=-1; j<=1; j++) {
+          //edge case check
+          if (y+i >= 0 && y+i < ROWS && x+j >= 0 && x+j < COLS) {
+            neighbours += grid[y+i][x+j];
+          }
+        }
+      }
+
+      //don't count self!
+      neighbours -= grid[y][x];
+
+      //apply rules
+      if (grid[y][x] === 1) { //alive
+        if (neighbours === 2 || neighbours === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if (grid[y][x] === 0) { //dead
+        if (neighbours === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+    }
+  }
+
+  return nextTurn;
+}
+
+function mousePressed() {
+  let xPos = Math.floor(mouseX/cellWidth);
+  let yPos = Math.floor(mouseY/cellHeight);
+
+  if (grid[yPos][xPos] === 0) {
+    grid[yPos][xPos] = 1;
+  }
+  else if (grid[yPos][xPos] === 1) {
+    grid[yPos][xPos] = 0;
+  }
 }
 
 function displayGrid(grid) {
@@ -57,63 +139,4 @@ function createRandom2dArray(COLS, ROWS) {
     }
   }
   return emptyArray;
-}
-
-function keyPressed(){
-  if (key === "e"){
-    grid = create2dArray(COLS, ROWS);
-  }
-  if (key === " "){
-    grid = takeTurn(grid);
-  }
-}
-
-function takeTurn(grid){
-  let nextTurn = create2dArray(COLS,ROWS);
-
-  for (let y = 0; y < ROWS; y++){
-    for (let x = 0; x < COLS; x++){
-      let neighbours = 0;
-
-      for (let i = -1; i <= 1; i++){
-        for (let j = -1; j <= 1; j++){
-
-          if (y+i >= 0 && y+i < ROWS && x +j >=0 && x+j < COLS){
-            neighbours += grid[y+i][x+j];
-          }
-        }
-      }
-
-      neighbours -= grid[y][x];
-      if (grid[y][x] === 1){
-        if (neighbours === 2 || neighbours === 3){
-          nextTurn[y][x] = 1;
-        }
-        else{
-          nextTurn[y][x] = 0;
-        }
-      }
-      if (grid[y][x] === 0){
-        if (neighbours === 3){
-          nextTurn[y][x] = 1;
-        }
-        else {
-          nextTurn[y][x] = 0;
-        }
-      }
-    }
-  }
-
-  return nextTurn;
-}
-function mousePressed() {
-  let xPos = Math.floor(mouseX/cellWidth);
-  let yPos = Math.floor(mouseY/cellHeight);
-
-  if (grid[yPos][xPos] === 0) {
-    grid[yPos][xPos] = 1;
-  }
-  else if (grid[yPos][xPos] === 1) {
-    grid[yPos][xPos] = 0;
-  }
 }
