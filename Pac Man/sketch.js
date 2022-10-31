61a36551a51f97cb3ae4dd923e473ac5529383ee
Kesur0403463 <0403463@sps-student.sk.ca>
2022-10-31 11:37:57 -0600

    Update sketch.js

--- Pac Man/sketch.js
+++ Pac Man/sketch.js
@@ -1,9 +1,18 @@
-// Project Title
-// Your Name
-// Date
+// Pac Man
+// Kati Kesur
+// October 31, 2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+// use 2d arrays in a different way then in class
+// go beyond the basic means of the project
+
+// defining global variables
+
+let PacX;
+let PacY;
+
 
 
 function setup() {
@@ -12,4 +21,27 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+function ShowPac(){
+  let Pac ={
+    x: PacX,
+    y: PacY,
+    colour: "yellow"
+  };
+}
+
+function movePac(){
+  if (keyIsDown(LEFT_ARROW)){
+    PacX += -5;
+  }
+  if (keyIsDown(RIGHT_ARROW)){
+    PacX += 5;
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    PacX += 5;
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    PacX += 5;
+  }
 }
