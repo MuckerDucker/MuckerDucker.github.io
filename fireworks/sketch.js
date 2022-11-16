641f09768fe984a7e618d7d2e4ccd30bdc140b47
Kesur0403463 <0403463@sps-student.sk.ca>
2022-11-16 12:03:06 -0600

    demo

--- fireworks/sketch.js
+++ fireworks/sketch.js
@@ -1,34 +1,40 @@
 // Fireworks
 // Kati
 
-class Particle{
-  constructor(x,y){
+class Particle {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.dx= random(-5,5);
+    this.dx = random(-5, 5);
     this.dy = random(-5, 5);
+    this.diameter = 5;
     this.r = 255;
     this.g = 0;
     this.b = 0;
     this.alpha = 255;
-    this.diameter = 3;
-    this.color = color(this.r, this.g, this.b);
+    this.color = color(this.r, this.g, this.b, this.alpha);
   }
 
-  update(){
+  update() {
+    //move
     this.x += this.dx;
     this.y += this.dy;
-    this.alpha --;
-    this.color = color(this.r, this.g, this.b);
+
+    //making it more transparent
+    this.alpha--;
+    this.color = color(this.r, this.g, this.b, this.alpha);
   }
 
-  display(){
+  display() {
     fill(this.color);
     stroke(this.color);
     circle(this.x, this.y, this.diameter);
   }
 }
 
+function isDead(){
+  return this.alpha <= 0;
+}
 let theFireworks = [];
 
 function setup() {
@@ -37,14 +43,20 @@ function setup() {
 
 function draw() {
   background("black");
-  for (let i = 0; i < theFireworks.length; i++){
+  for (let i = 0; i < theFireworks.length; i++) {
     theFireworks[i].update();
-    theFireworks[i].display();
+    if (theFireworks[i].isDead()){
+      // remove from array 
+      theFireworks.splice(i, 1);
+    }
+    else{
+      theFireworks[i].display();
+    }
   }
 }
 
-function mousepressed(){
-  for(let i = 0; i < 100; i ++){
+function mousePressed() {
+  for (let i = 0; i < 100; i++) {
     let someParticle = new Particle(mouseX, mouseY);
     theFireworks.push(someParticle);
   }
